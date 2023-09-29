490e753f36d076645b766bfd41b6eff356ff5b3d
zf <zdenek.frydryn@gmail.com>
2023-09-29 21:01:02 +0200

    Working solution for problem #1

--- ukoly/1.ukol/ZSP_Ukol-1.cpp
+++ ukoly/1.ukol/ZSP_Ukol-1.cpp
@@ -6,6 +6,42 @@
 void u1_1()
 {
     // todo u1-1
+    // 
+    // input 1
+    // pocet ks [int]
+    // input 2
+    // cena za ks bez DPH [float]
+    // 
+    // output
+    // ---------------------
+    // Účtenka
+    // Cena bez DPH/ks 200 Kč<tab>Cena s DPH/ks 240 Kč
+    // Počet kusů: 5<tab>Cena bez DPH 1000 Kč<tab>Cena s DPH (20 %)
+    // 1400 Kč
+    // ---------------------
+
+    int pocet;
+    const float DPH = 1.2;
+    const int DPHproc = (DPH -1) * 100;
+    double cena;
+    double cena_s_DPH;
+    double celkem;
+    double celkem_s_DPH;
+
+    printf("Zadejte pocet ks polozky:\n");
+    scanf("%i", &pocet);
+    printf("Zadejte cenu za 1ks polozky:\n");
+    scanf("%lf", &cena);
+
+    cena_s_DPH = cena * DPH;
+    celkem = pocet * cena;
+    celkem_s_DPH = celkem * DPH;
+
+    printf("Účtenka\n");
+    printf("Cena bez DPH/ks %.2lf Kč\tCena s DPH/ks %.2lf Kč\n", cena, cena_s_DPH);
+    printf("Počet kusů: %i\tCena bez DPH %.2lf Kč\tCena s DPH (%i %%) %.2lf\n", pocet, celkem, DPHproc, celkem_s_DPH);
+
+
 }
 
 void u1_2()
@@ -21,7 +57,7 @@ void u1_3()
 
 int main()
 {
-    ;
+    u1_1();
 }
 
 // Spuštění programu: Ctrl+F5 nebo nabídka Ladit > Spustit bez ladění
